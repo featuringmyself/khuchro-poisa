e63126191c4a73edd27a69970798b9afd55ee912
Rudra <110037323+featuringmyself@users.noreply.github.com>
2024-10-14 22:52:43 +0530

    changed to postgres

--- khuchro_poisa/settings.py
+++ khuchro_poisa/settings.py
@@ -88,8 +88,14 @@ WSGI_APPLICATION = 'khuchro_poisa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'FWuhTPgIsjyIwPOKiSDGhfzldOdVwKXq',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '21153',
         }
 }
 
